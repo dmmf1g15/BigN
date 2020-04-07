0a5af34eeb6d247aead8dcd82e871e45e7fbce29
McKay Fletcher D.M <dmmf1g15@soton.ac.uk>
2020-04-07 10:27:49 +0100

    added .py file

---
+++ gen_model.py
@@ -0,0 +1,17 @@
+import os
+
+
+
+
+def gen_next_model():
+
+
+
+if __name__=="__main__":
+
+#(rain,iterate,computer,port)
+
+#this code is to generate shell script for either duncan roose or iridis. If it is run local nothing is made
+#this code will also pull the relevant rain_data
+
+
