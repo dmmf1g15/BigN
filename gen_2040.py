34bd03044700a1f62e14427691ccdc4276861c47
D.M.McKay-Fletcher <dmmf1g15@cyan01.(none)>
2020-12-10 14:59:48 +0000

    fixed script

--- gen_2040.py
+++ gen_2040.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 	port=2082
 	for i in range(32041,32050):
 		
-		for j in range(1,3)
+		for j in range(1,3):
 			gen_model.gen_script("Rain_G_"+str(i)+".txt","iter"+str(j),"iridis",port)
 			time.sleep(0.5)
 			port+=1
