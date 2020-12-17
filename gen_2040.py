bcce858d66e2d3a820029412a1b50752667c34ef
D.M.McKay-Fletcher <dmmf1g15@cyan01.(none)>
2020-12-17 12:36:07 +0000

    gen_2040 update

--- gen_2040.py
+++ gen_2040.py
@@ -3,8 +3,8 @@ import time
 
 
 if __name__ == "__main__":
-	port=2082
-	for i in range(32041,32050):
+	port=2056
+	for i in range(32040,32050):
 		
 		for j in range(1,3):
 			gen_model.gen_script("Rain_"+str(i)+".txt","iter"+str(j),"iridis",port)
