2931b7ae674d6d505b67b03c78c74502a34d453d
D.M.McKay-Fletcher <dmmf1g15@cyan01.(none)>
2021-01-04 13:33:26 +0000

    updated gen 2040

--- gen_2040.py
+++ gen_2040.py
@@ -4,9 +4,31 @@ import time
 
 if __name__ == "__main__":
 	port=2056
-	for i in range(32040,32050):
-		
-		for j in range(1,3):
-			gen_model.gen_script("Rain_"+str(i)+".txt","iter"+str(j),"iridis",port)
-			time.sleep(0.5)
-			port+=1
+	
+	gen_model.gen_script("Rain_32040.txt","iter1","iridis",port)
+	time.sleep(0.5)
+	port+=1
+
+	gen_model.gen_script("Rain_32040.txt","iter2","iridis",port)
+	time.sleep(0.5)
+	port+=1
+
+    gen_model.gen_script("Rain_32041.txt","iter2","iridis",port)
+	time.sleep(0.5)
+	port+=1
+
+	gen_model.gen_script("Rain_32043.txt","iter2","iridis",port)
+	time.sleep(0.5)
+	port+=1
+
+	gen_model.gen_script("Rain_32044.txt","iter2","iridis",port)
+	time.sleep(0.5)
+	port+=1
+
+	gen_model.gen_script("Rain_32048.txt","iter1","iridis",port)
+	time.sleep(0.5)
+	port+=1
+
+	gen_model.gen_script("Rain_32049.txt","iter1","iridis",port)
+	time.sleep(0.5)
+	port+=1
